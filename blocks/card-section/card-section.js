4893b559f3cfc1ed29f0eb1fb5d21c7bbf27542b
Sivapurani17 <sivapurani@gmail.com>
2026-08-20 22:43:40 +0530

    Es lint fix

--- blocks/card-section/card-section.js
+++ blocks/card-section/card-section.js
@@ -1,14 +1,12 @@
-export default function decorate(block)
-{
-    const cards=[...block.children];
-    cards.forEach((card) =>
-    {
-        card?.classList.add('card-sectiondiv');
-        const cardimg= card.querySelector('p:has(img)');
-        cardimg?.classList.add('card-sectiondiv__image');
-        const cardtitle= card.querySelector('h2');
-        cardtitle?.classList.add('card-sectiondiv__title');
-        const carddescription= card.querySelector('p:not(:has(img))');
-        carddescription?.classList.add('card-sectiondiv__description');
-    });
+export default function decorate(block) {
+  const cards = [...block.children];
+  cards.forEach((card) => {
+    card?.classList.add('card-sectiondiv');
+    const cardimg = card.querySelector('p:has(img)');
+    cardimg?.classList.add('card-sectiondiv__image');
+    const cardtitle = card.querySelector('h2');
+    cardtitle?.classList.add('card-sectiondiv__title');
+    const carddescription = card.querySelector('p:not(:has(img))');
+    carddescription?.classList.add('card-sectiondiv__description');
+  });
 }
